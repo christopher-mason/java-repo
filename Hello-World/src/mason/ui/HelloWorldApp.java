82c2ac1d755ef9b5fe4e244d0d83f5f646486f79
Chris Mason <m1achris43@gmail.com>
2021-02-11 22:13:23 -0500

    init commit

---
+++ Hello-World/src/mason/ui/HelloWorldApp.java
@@ -0,0 +1,18 @@
+package mason.ui;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println();
+		
+		int x = 3;
+		int y = 4;
+		int result = x + y;
+		
+		System.out.println(result);
+		System.out.println(x + y);
+
+	}
+
+}
